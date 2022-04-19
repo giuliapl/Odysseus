6efaa6e4edc09ffc772859f29ace1d39eb442aca
giulia_p_lib <libralessogiulia@gmail.com>
2022-04-19 21:51:47 +0200

    sviluppo prime logiche di Game.js

--- src/screens/game/Game.js
+++ src/screens/game/Game.js
@@ -10,13 +10,20 @@ class Game extends Component {
     constructor(props) {
         super(props)
 
-        //[isPlaying, setIsPlaying] = useState(false);
 
-        this.isPlaying = true;
+        this.state = {
+            isPlaying: false,
+            avatarPosition: 180
+        }
 
+        this.range = this.range.bind(this);
 
     }
 
+    GRAVITY = 15;
+
+
+    //calcola l'area di gioco in altezza
     range(start, end) {
         if (start === end) return [start];
         return [start, ...this.range(start + 1, end)];
@@ -26,28 +33,101 @@ class Game extends Component {
     playableArea = () => {
         let windowHeight = window.innerHeight;
         let rangeY = this.range(106, windowHeight - 94);
-        console.log('window height:', windowHeight);
-        console.log('rangeY:', rangeY);
+    }
+
+
+    componentDidMount() {
+        //Scheduler che simula la gravità
+        this.fall = setInterval(() => {
+            let obj = this.state;
+
+            //Simulo la gravità
+            obj.avatarPosition = obj.avatarPosition + this.GRAVITY;
+
+            //Controllo se l'avatar è ancora nella playable area: se non lo è, game over
+            if ((106 > obj.avatarPosition) || (obj.avatarPosition > window.innerHeight - 94)) {
+                obj.isPlaying = false; //METTERE MODALE GAME OVER
+            }
+
+            this.setState(
+                obj
+            )
+
+        }, 25);
+    }
+
+
+    componentWillUnmount() {
+        clearInterval(this.fall);
+        this.state = {
+            isPlaying: false,
+            avatarPosition: 180
+        }
+    }
+
+
+
+
+    //Event handlers
+
+    //fa partire il gioco
+    handleStartGame = () => {
+        // let obj = this.state;
+
+        this.state.isPlaying = true;
+        this.state.avatarPosition = 180;
+
 
     }
 
-    render() {
 
-        if (!this.isPlaying) {
-            return (
-                <>
-                    bottone per iniziare a giocare
-                </>
-            )
-        } else {
-            return (
-                <>
-                    <div className="game-container" onClick={this.playableArea}>
-                        <Odysseus positionY={200} />
+    render() {
+        return (
+            <>
+
+                {
+                    !this.state.isPlaying &&
+                    <div className="wanna-play-container">
+                        <header>
+                            <h1>Let's go!</h1>
+                        </header>
+                        <button onClick={this.handleStartGame}>
+                            Start Game
+                        </button>
                     </div>
-                </>
-            )
-        }
+                }
+
+                {
+                    this.state.isPlaying &&
+                    <div>
+                        <button>Ranking</button>
+                        <div className="game-container" onClick={this.playableArea}>
+                            <Odysseus positionY={this.state.avatarPosition} />
+                        </div>
+                    </div>
+                }
+            </>
+
+        )
+
+        /*   if (this.state.isPlaying !== true) {
+               return (
+                   <>
+                       <button onClick={this.handleStartGame}>
+                           Start Game
+                       </button>
+                   </>
+               )
+           } else if (this.state.isPlaying === true) {
+               return (
+                   <>
+                       <div className="game-container" onClick={this.playableArea}>
+                           <Odysseus positionY={200} />
+                       </div>
+                   </>
+               )
+           }
+           */
     }
 
 }
@@ -84,8 +164,8 @@ STATI:
 --- show --> o true o false
 ------ inizializzato a false
 ------ diventa true ogni tot di secondi
------- se sulla y coincidono, game over
------- dopo tot secondi sposta la position a sinistra di totpx (translate-x)
+------ se la x dell'ostacolo è uguale a quella di Ulisse e sulla y coincidono, game over
+------ dopo tot secondi sposta la position a sinistra di tot px (translate-x)
 ------ quando arriva a x=0, show diventa false, image e position cambiano e delay viene riassegnato
 --- image --> array di stringhe, scegli random
 --- position
