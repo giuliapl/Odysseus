a244990aac13bf647b934c89ee8d3877dc6c32d5
Laptop <lidia.cirrone@gmail.com>
2022-04-25 23:55:33 +0200

    refactor

--- src/components/funcComponents/ui/obstacle/Obstacle.js
+++ src/components/funcComponents/ui/obstacle/Obstacle.js
@@ -6,75 +6,29 @@ import './Obstacle.css';
 
 function Obstacle(props) {
 
-    useEffect(() => {
-        let obj = document.querySelector('.obstacle img').getBoundingClientRect();
-        props.setObstacleSize(
-            obj.width,
-            obj.height
-        );
-    }, [])
+   useEffect(() => {
+      let obj = document.querySelector('.obstacle img').getBoundingClientRect();
+      props.setObstacleSize(
+         obj.width,
+         obj.height
+      );
+   }, [])
 
-    return (
-
-        <>
-            <div
-                style={{
-                    transform: `translateY(${props.positionY}px)`,
-                    backgroundColor: `purple`,
-                    position: `absolute`,
-                    width: `8px`,
-                    height: `8px`,
-                }}
-            >
-                Y
-            </div>
-            <div
-                style={{
-                    transform: `translateX(${props.positionX}px)`,
-                    backgroundColor: `orange`,
-                    position: `absolute`,
-                    width: `8px`,
-                    height: `8px`
-                }}
-            >
-                X
-            </div>
-            <div
-                style={{
-                    transform: `translateY(${props.h}px)`,
-                    backgroundColor: `black`,
-                    position: `absolute`,
-                    width: `8px`,
-                    height: `8px`,
-                }}
-            >
-                H
-            </div>
-            <div
-                style={{
-                    transform: `translateX(${props.w}px)`,
-                    backgroundColor: `gray`,
-                    position: `absolute`,
-                    width: `8px`,
-                    height: `8px`,
-                }}
-            >
-                W
-            </div>
-            <div
-                className="obstacle"
-                style={{
-                    transform: `translate(${props.positionX}px, ${props.positionY}px)`
-                }}
-            >
-                <picture>
+   return (
+      <>
+         <div
+            className="obstacle"
+            style={{
+               transform: `translate(${props.positionX}px, ${props.positionY}px)`
+            }}
+         >
+            <picture>
                <img src={props.iconSrc} />
-                </picture>
+            </picture>
 
-            </div>
-        </>
-
-    )
+         </div>
+      </>
+   )
 }
 
 export default Obstacle;
--- src/components/funcComponents/ui/odysseus/Odysseus.js
+++ src/components/funcComponents/ui/odysseus/Odysseus.js
@@ -6,85 +6,34 @@ import './Odysseus.css';
 
 function Odysseus(props) {
 
-    const ODYSSEUS_HEIGHT = 512;
-    const ODYSSEUS_WIDTH = 397;
+   const ODYSSEUS_HEIGHT = 512;
+   const ODYSSEUS_WIDTH = 397;
 
-    useEffect(() => {
-        let obj = document.querySelector('.odysseus img').getBoundingClientRect();
-        let height = obj.height;
-        let width = obj.width;
-        if (height === 0 && width > 0) {
-            height = (ODYSSEUS_HEIGHT * width) / ODYSSEUS_WIDTH
-        }
-        props.setOdysseusSize(
-            height,
-            width
-        );
-    }, [])
+   useEffect(() => {
+      let obj = document.querySelector('.odysseus img').getBoundingClientRect();
+      let height = obj.height;
+      let width = obj.width;
+      props.setOdysseusSize(
+         height,
+         width
+      );
+   }, [])
 
-    return (
+   return (
+      <>
+         <div
+            className="odysseus"
+            style={{
+               transform: `translateY(${props.positionY}px)`
+            }}
+         >
+            <picture>
+               <img src={require('../../../../assets/icons/odysseus.png')} />
+            </picture>
 
-        <>
-            <div
-                style={{
-                    transform: `translateY(${props.positionY}px)`,
-                    backgroundColor: `blue`,
-                    position: `absolute`,
-                    width: `8px`,
-                    height: `8px`,
-                }}
-            >
-                Y
-            </div>
-            <div
-                style={{
-                    transform: `translateX(${props.x}px)`,
-                    backgroundColor: `green`,
-                    position: `absolute`,
-                    width: `8px`,
-                    height: `8px`
-                }}
-            >
-                X
-            </div>
-            <div
-                style={{
-                    transform: `translateY(${props.h}px)`,
-                    backgroundColor: `red`,
-                    position: `absolute`,
-                    width: `8px`,
-                    height: `8px`,
-                }}
-            >
-                H
-            </div>
-            <div
-                style={{
-                    transform: `translateX(${props.w}px)`,
-                    backgroundColor: `yellow`,
-                    position: `absolute`,
-                    width: `8px`,
-                    height: `8px`,
-                }}
-            >
-                W
-            </div>
-            <div
-                className="odysseus"
-                style={{
-                    transform: `translateY(${props.positionY}px)`
-                }}
-            >
-
-
-                <picture>
-                    <img src={require('../../../../assets/icons/odysseus.png')} />
-                </picture>
-
-            </div>
-        </>
-
-    )
+         </div>
+      </>
+   )
 }
 
 export default Odysseus;
